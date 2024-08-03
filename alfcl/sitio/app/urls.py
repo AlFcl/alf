1371beef7bd31b85d619207ddcd05296cb50abed
alfcl <gonzalo.loyola@gloyola.cl>
2024-08-03 19:21:04 -0400

    ya con un poco mas de forma  s instala theme

--- alfcl/sitio/app/urls.py
+++ alfcl/sitio/app/urls.py
@@ -1,12 +1,10 @@
-# app/urls.py
 from django.urls import path
-from .views import home, github, quien_soy, contacto, blog, post_detail
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('github/', github, name='github'),
-    path('quien-soy/', quien_soy, name='quien_soy'),
-    path('contacto/', contacto, name='contacto'),
-    path('blog/', blog, name='blog'),
-    path('blog/<str:filename>/', post_detail, name='post_detail'),
+    path('', views.home, name='home'),
+    path('blog/', views.blog, name='blog'),
+    path('github/', views.github, name='github'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('quien-soy/', views.quien_soy, name='quien_soy'),
 ]
--- alfcl/sitio/sitio/settings.py
+++ alfcl/sitio/sitio/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'sitio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'app/templates')],  # Añadir esta línea
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,9 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
